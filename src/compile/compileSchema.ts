21d5607bd7b3262e83042ae3c5f584ea2c0f8c76
alexharri <alexharri2919@gmail.com>
2022-10-02 12:24:36 +0200

    feat(compileSchema): connect validateObject to compileSchema

--- src/compile/compileSchema.ts
+++ src/compile/compileSchema.ts
@@ -1,12 +1,19 @@
 import { astFromString } from "../ast/astFromString";
 import { Parse } from "../types";
 import { Schema } from "../types/Schema";
+import { validateObject } from "../validate/object";
 
 export function compileSchema<T extends string>(template: T): Schema<Parse<T>> {
   const ast = astFromString(template);
 
   const schema: Schema<Parse<T>> = {
-    parseSync: (value) => {},
+    parseSync: (value) => {
+      const errorMessage = validateObject(value, ast.properties);
+      if (errorMessage) {
+        throw new Error(errorMessage);
+      }
+      return value as Parse<T>;
+    },
   };
 
   return schema;
