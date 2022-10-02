c7702b32affa6c04392b0cea1f7f22e738cd57c0
alexharri <alexharri2919@gmail.com>
2022-10-02 12:48:46 +0200

    test(compileSchema): test that parseSync does not pass values provided to it as-is

--- src/compile/compileSchema.spec.ts
+++ src/compile/compileSchema.spec.ts
@@ -1,12 +1,24 @@
+import { SchemaValue } from "../types/Schema";
 import { compileSchema } from "./compileSchema";
 
 describe("compileSchema", () => {
   it("successfully compiles a schema and parses a value", () => {
     const schema = compileSchema(`{ a: string; b: number; }`);
-    const value = { a: "Hello", b: 50 };
+    const value: SchemaValue<typeof schema> = { a: "Hello", b: 50 };
 
     const parsed = schema.parseSync(value);
 
     expect(parsed).toEqual(value);
+  });
+
+  it("copies the provided data instead of passing it as-is", () => {
+    const schema = compileSchema(`{ a: { b: string; }; }`);
+    const value: SchemaValue<typeof schema> = { a: { b: "Hello" } };
+
+    const parsed = schema.parseSync(value);
+
+    expect(parsed).toEqual(value);
+    expect(parsed === value).toEqual(false);
+    expect(parsed.a === value.a).toEqual(false);
   });
 });
