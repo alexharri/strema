fb7b476d324c12ec175339033ff055b09d3efda0
alexharri <alexharri2919@gmail.com>
2022-10-02 12:11:22 +0200

    feat(compileSchema): add validateObject

---
+++ src/validate/object.ts
@@ -0,0 +1,39 @@
+import { typeAsString } from "../format/typeAsString";
+import { enforceExhaustive } from "../switch";
+import { PropertyNode } from "../types/Ast";
+
+/**
+ * @returns empty string if valid
+ */
+export function validateObject(
+  obj: unknown,
+  properties: PropertyNode[]
+): string {
+  const isNotObjectValue = typeof obj !== "object";
+  const isNullOrUndefined = obj === undefined || obj === null;
+
+  if (isNullOrUndefined) {
+    return "";
+  }
+
+  if (isNotObjectValue) {
+    return `Expected object value, got ${typeAsString(obj)}`;
+  }
+
+  for (const { key, value: valueSpec } of properties) {
+    const { type } = valueSpec;
+    const value = (obj as Record<string, unknown>)[key];
+    switch (type) {
+      case "object":
+        return validateObject(value, valueSpec.properties);
+      case "array":
+        throw new Error(`Not implemented`);
+      case "primitive":
+        throw new Error(`Not implemented`);
+      default:
+        enforceExhaustive(type, "Unexpected value type");
+    }
+  }
+
+  return "";
+}
