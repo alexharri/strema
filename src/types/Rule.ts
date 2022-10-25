a29e71ae375b81bf73e21e6f956b74c299d879c8
alexharri <alexharri2919@gmail.com>
2022-10-25 13:58:41 +0000

    feat(string-rules): add validation logic for various string rules

--- src/types/Rule.ts
+++ src/types/Rule.ts
@@ -1,6 +1,15 @@
 export type StringEmailRule = { type: "email" };
+export type StringMinRule = { type: "min"; value: number };
+export type StringMaxRule = { type: "max"; value: number };
+export type StringLengthRule = { type: "length"; value: number };
+export type StringUuidRule = { type: "uuid" };
 
-export type StringRule = StringEmailRule;
+export type StringRule =
+  | StringEmailRule
+  | StringMinRule
+  | StringMaxRule
+  | StringLengthRule
+  | StringUuidRule;
 
 export type NumberIntRule = { type: "int" };
 export type NumberPositiveRule = { type: "positive" };
